602c02ca02fb89513e7aa0b6861fc43be00821c5
Yator1 <titusyatz17@gmail.com>
2024-10-03 18:13:04 +0300

    Methods, static methods...

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,40 @@
+export default class Currency {
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
+    }
+
+    validateName(name){
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        return name;
+    }
+
+    validateCode(code){
+        if (typeof code !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        return code;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(newName){
+        this._name = this.validateName(newName)
+    }
+
+    get code(){
+        return this._code;
+    }
+
+    set code(newCode){
+        this._code = this.validateCode(newCode)
+    }
+
+    displayFullCurrency(){
+        return `${this._name} (${this._code})`;
+    }
+}
