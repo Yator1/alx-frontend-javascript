8611dc1edf36dc79502d1da947c7721ee33c2548
Yator1 <titusyatz17@gmail.com>
2024-10-07 17:16:16 +0300

    promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,9 +2,9 @@ export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     const success = true;
     if (success) {
-      resolve("API response received successfully!");
+      resolve('API response received successfully!');
     } else {
-      reject("Failed to get response from API.");
+      reject('Failed to get response from API.');
     }
   });
 }
